f4d39ad54d0463c696320812bd2a01019cc4332b
nadim249 <mahmoudnadim47@gmail.com>
2026-05-09 17:29:37 +0600

    Design:fix icon in bottom nav and post Fragment

--- LostAndFound/app/src/main/java/com/example/lostandfound/MainActivity.kt
+++ LostAndFound/app/src/main/java/com/example/lostandfound/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -28,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_home -> loadFragment(HomeFragment())
-                //R.id.nav_post -> loadFragment(PostFragment())
+                R.id.nav_post -> loadFragment(PostFragment())
                 //R.id.nav_chat -> loadFragment(ChatFragment())
                // R.id.nav_notification -> loadFragment(NotificationFragment())
             }
